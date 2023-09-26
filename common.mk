a40caa43351c2a418c7167383414320acaca9ee5
0mar99 <omarantonio9972@gmail.com>
2023-09-26 14:29:31 -0600

    sm8350-common: Blaze-ify!

--- common.mk
+++ common.mk
@@ -446,7 +446,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-bliss
+    $(LOCAL_PATH)/overlay-blaze
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
