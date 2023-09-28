d09f1e0c48835d5d4d384722e4443a47a4df7b38
0mar99 <omarantonio9972@gmail.com>
2023-09-28 12:55:50 -0600

    sm8350-common: Drop foreground and top-app cpus

--- rootdir/bin/init.kernel.post_boot-lahaina.sh
+++ rootdir/bin/init.kernel.post_boot-lahaina.sh
@@ -190,7 +190,6 @@ echo 325 > /proc/sys/kernel/walt_low_latency_task_threshold
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/restricted/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
-echo 0-6 > /dev/cpuset/foreground/cpus
 echo 0-3 > /dev/cpuset/audio-app/cpus
 
 # Turn off scheduler boost at the end
