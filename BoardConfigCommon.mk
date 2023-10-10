5bd7d2fd5722f0ab70cbe6aa4d8c03862c0afbd2
ralph950412 <ralph950412@gmail.com>
2023-10-10 14:48:00 +0800

    [DNM] sm8350-common: Permissive SELinux
    Change-Id: Ied9fc1faa5182bef99986c7e5db4f6a14895eb43

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,6 +110,7 @@ BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # LiveDisplay
 ifneq ("$(wildcard hardware/lineage/livedisplay)", "")
